e3e3389bdde9f5a953573cc8e495aff6013d4c38
Axowil <welfordwillian@gmail.com>
2026-06-16 11:10:08 -0500

    fix it 1

--- view-project/vite.config.js
+++ view-project/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue' // <--- CORREGIDO AQUÍ
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
+      // Detecta el prefijo que pusiste en tu Vue
       '/api-backend': {
         target: 'https://sisacad-enrollments-backend.vercel.app',
         changeOrigin: true,
+        secure: false,
+        // Esto elimina '/api-backend' al pasar la petición al backend original
         rewrite: (path) => path.replace(/^\/api-backend/, '')
       }
     }
